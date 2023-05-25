cc3c89263374c30746db44bc8ff95b2691c72811
Steven <stevengf14@gmail.com>
2023-05-25 00:12:24 -0500

    First commit - Creating springboot-reactor project

---
+++ springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
@@ -0,0 +1,13 @@
+package ec.com.learning.reactor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactorApplication.class, args);
+	}
+
+}
---
+++ springboot-reactor/src/test/java/ec/com/learning/reactor/app/SpringbootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.learning.reactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
