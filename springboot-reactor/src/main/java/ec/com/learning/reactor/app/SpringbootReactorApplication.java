3717916b89fda918bfc0695991a0237dcc52409e
Steven <stevengf14@gmail.com>
2023-05-26 00:08:17 -0500

    subscribe() method

--- springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
+++ springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
@@ -1,5 +1,7 @@
 package ec.com.learning.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +11,21 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringbootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> names = Flux.just("Andres", "Pedro", "Diego", "Juan").doOnNext(System.out::println);
-		names.subscribe();
+		Flux<String> names = Flux.just("Andres", "Pedro", "", "Diego", "Juan").doOnNext(e -> {
+			if (e.isEmpty()) {
+				throw new RuntimeException("Names can't be empty");
+			}
+			System.out.println(e);
+		});
+		names.subscribe(e -> log.info(e), error -> log.error(error.getMessage()));
 
 	}
 
