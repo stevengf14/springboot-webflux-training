81fbaf3370627c6bff9e244e2d5872c77fac6018
Steven <stevengf14@gmail.com>
2023-05-25 23:39:09 -0500

    creating a stream flux observable

--- springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
+++ springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
@@ -1,13 +1,23 @@
 package ec.com.learning.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringbootReactorApplication {
+public class SpringbootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> names = Flux.just("Andres", "Pedro", "Diego", "Juan").doOnNext(System.out::println);
+		names.subscribe();
+
 	}
 
 }
