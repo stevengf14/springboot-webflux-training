1d99d3363b7d354017ebbc13530888649fa0daa1
Steven <stevengf14@gmail.com>
2023-06-01 22:11:32 -0500

    create operator

--- springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
+++ springboot-reactor/src/main/java/ec/com/learning/reactor/app/SpringbootReactorApplication.java
@@ -3,6 +3,8 @@ package ec.com.learning.reactor.app;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
 
 import org.slf4j.Logger;
@@ -38,7 +40,33 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 		// userCommentsZipRangesWithExample();
 		// intervalExample();
 		// delayElementsExample();
-		infiniteIntervalExample();
+		// infiniteIntervalExample();
+		intervalFromCreateExample();
+	}
+
+	public void intervalFromCreateExample() {
+		Flux.create(emitter -> {
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+
+				private Integer counter = 0;
+
+				@Override
+				public void run() {
+					emitter.next(++counter);
+					if (counter == 10) {
+						timer.cancel();
+						emitter.complete();
+					}
+
+					if (counter == 5) {
+						timer.cancel();
+						emitter.error(new InterruptedException("Error, we stop flux at 5!"));
+					}
+				}
+			}, 1000, 1000);
+		}).subscribe(next -> log.info(next.toString()), error -> log.error(error.getMessage()),
+				() -> log.info("Process finished!"));
 	}
 
 	public void infiniteIntervalExample() throws InterruptedException {
