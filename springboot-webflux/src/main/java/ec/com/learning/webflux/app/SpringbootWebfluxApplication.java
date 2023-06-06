28123e9b5efb3b7521cdf4b876ddf5726594832d
Steven <stevengf14@gmail.com>
2023-06-05 22:50:45 -0500

    change database name and createAt

--- springboot-webflux/src/main/java/ec/com/learning/webflux/app/SpringbootWebfluxApplication.java
+++ springboot-webflux/src/main/java/ec/com/learning/webflux/app/SpringbootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package ec.com.learning.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +32,16 @@ public class SpringbootWebfluxApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		mongoTemplate.dropCollection("products").subscribe();
-		
+
 		Flux.just(new Product("TV Panasonic LCD", 456.89), new Product("Xiaomi Redmi Note Pro 11", 350.50),
 				new Product("IPod touch 256GB", 356.45), new Product("IPad mini 256GB", 850.00),
 				new Product("Canon T7i", 650.25), new Product("Asus ROG Strix", 1950.75),
 				new Product("TV  TCL 55", 750.25), new Product("Monitor LG 55", 450.72),
 				new Product("Lenovo Yoga Tab 128GB", 230.25), new Product("Mouse + Keyboard", 15.43))
-				.flatMap(product -> dao.save(product))
-				.subscribe(product -> log.info("Insert:" + product.getId() + " " + product.getName()));
+				.flatMap(product -> {
+					product.setCreateAt(new Date());
+					return dao.save(product);
+				}).subscribe(product -> log.info("Insert:" + product.getId() + " " + product.getName()));
 
 	}
 
