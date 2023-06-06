1e8e5d3e7841d8592a2903c2adcef2213e3ba530
Steven <stevengf14@gmail.com>
2023-06-05 23:14:50 -0500

    subscribing another observer and modifying active Stream

--- springboot-webflux/src/main/java/ec/com/learning/webflux/app/controller/ProductController.java
+++ springboot-webflux/src/main/java/ec/com/learning/webflux/app/controller/ProductController.java
@@ -1,10 +1,11 @@
 package ec.com.learning.webflux.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import ec.com.learning.webflux.app.models.dao.ProductDao;
 import ec.com.learning.webflux.app.models.documents.Product;
 import reactor.core.publisher.Flux;
@@ -15,9 +16,16 @@ public class ProductController {
 	@Autowired
 	private ProductDao dao;
 
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+
 	@GetMapping({ "/list", "/" })
 	public String list(Model model) {
-		Flux<Product> products = dao.findAll();
+		Flux<Product> products = dao.findAll().map(product -> {
+			product.setName(product.getName().toUpperCase());
+			return product;
+		});
+		products.subscribe(prod -> log.info(prod.getName()));
+		
 		model.addAttribute("products", products);
 		model.addAttribute("title", "List of products");
 		return "list";
