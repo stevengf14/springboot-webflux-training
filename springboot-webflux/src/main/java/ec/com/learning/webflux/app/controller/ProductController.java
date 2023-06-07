fb8071c67021047759acdc6298de490735771be8
Steven <stevengf14@gmail.com>
2023-06-06 21:55:50 -0500

    Reactive Data Driver

--- springboot-webflux/src/main/java/ec/com/learning/webflux/app/controller/ProductController.java
+++ springboot-webflux/src/main/java/ec/com/learning/webflux/app/controller/ProductController.java
@@ -1,11 +1,15 @@
 package ec.com.learning.webflux.app.controller;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
+
 import ec.com.learning.webflux.app.models.dao.ProductDao;
 import ec.com.learning.webflux.app.models.documents.Product;
 import reactor.core.publisher.Flux;
@@ -25,10 +29,22 @@ public class ProductController {
 			return product;
 		});
 		products.subscribe(prod -> log.info(prod.getName()));
-		
+
 		model.addAttribute("products", products);
 		model.addAttribute("title", "List of products");
 		return "list";
 	}
 
+	@GetMapping("/list-datadriver")
+	public String listDataDriver(Model model) {
+		Flux<Product> products = dao.findAll().map(product -> {
+			product.setName(product.getName().toUpperCase());
+			return product;
+		}).delayElements(Duration.ofSeconds(1));
+		products.subscribe(prod -> log.info(prod.getName()));
+
+		model.addAttribute("products", new ReactiveDataDriverContextVariable(products, 1));
+		model.addAttribute("title", "List of products");
+		return "list";
+	}
 }
