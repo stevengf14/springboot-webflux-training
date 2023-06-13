f90431e09f7b18143f11cc60a5c3c49eac87b8f3
Steven <stevengf14@gmail.com>
2023-06-12 23:47:23 -0500

    handler methods: create and save

--- springboot-webflux/src/main/java/ec/com/learning/webflux/app/controller/ProductController.java
+++ springboot-webflux/src/main/java/ec/com/learning/webflux/app/controller/ProductController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 
 import ec.com.learning.webflux.app.models.documents.Product;
 import ec.com.learning.webflux.app.models.services.ProductService;
 import io.netty.handler.stream.ChunkedStream;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class ProductController {
@@ -24,13 +26,27 @@ public class ProductController {
 	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
 	@GetMapping({ "/list", "/" })
-	public String list(Model model) {
+	public Mono<String> list(Model model) {
 		Flux<Product> products = service.findAllWithUpperCaseName();
 		products.subscribe(prod -> log.info(prod.getName()));
 
 		model.addAttribute("products", products);
 		model.addAttribute("title", "List of products");
-		return "list";
+		return Mono.just("list");
+	}
+
+	@GetMapping("/form")
+	public Mono<String> create(Model model) {
+		model.addAttribute("product", new Product());
+		model.addAttribute("title", "Product form");
+		return Mono.just("form");
+	}
+
+	@PostMapping("/form")
+	public Mono<String> save(Product product) {
+		return service.save(product).doOnNext(p -> {
+			log.info("Product saved: " + p.getName() + " Id: " + p.getId());
+		}).thenReturn("redirect;/list");
 	}
 
 	@GetMapping("/list-datadriver")
