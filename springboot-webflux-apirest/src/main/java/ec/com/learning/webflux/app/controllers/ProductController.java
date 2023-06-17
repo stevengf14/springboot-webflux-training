51ef709a4de4874f547e368b9f1c93bbacfa3259
Steven <stevengf14@gmail.com>
2023-06-16 21:41:05 -0500

    handler get

--- springboot-webflux-apirest/src/main/java/ec/com/learning/webflux/app/controllers/ProductController.java
+++ springboot-webflux-apirest/src/main/java/ec/com/learning/webflux/app/controllers/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,11 @@ public class ProductController {
 	@GetMapping
 	public Mono<ResponseEntity<Flux<Product>>> list() {
 		return Mono.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(service.findAll()));
+	}
+
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Product>> get(@PathVariable String id) {
+		return service.findById(id).map(p -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(p))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 }
