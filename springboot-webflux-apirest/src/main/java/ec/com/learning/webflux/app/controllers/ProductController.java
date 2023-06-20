922721ced8d2bbb1f679ef492b63485b511c38f7
Steven <stevengf14@gmail.com>
2023-06-20 00:07:11 -0500

    delete handler - delete

--- springboot-webflux-apirest/src/main/java/ec/com/learning/webflux/app/controllers/ProductController.java
+++ springboot-webflux-apirest/src/main/java/ec/com/learning/webflux/app/controllers/ProductController.java
@@ -4,8 +4,10 @@ import java.net.URI;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +62,19 @@ public class ProductController {
 		}).map(p -> ResponseEntity.created(URI.create("/api/products/".concat(p.getId())))
 				.contentType(MediaType.APPLICATION_JSON_UTF8).body(p))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	/*
+	 * @DeleteMapping("/{id}") public Mono<ResponseEntity<Object>>
+	 * delete(@PathVariable String id) { return service.findById(id).flatMap(p -> {
+	 * return service.delete(p).then(Mono.just(ResponseEntity.noContent().build()));
+	 * }).defaultIfEmpty(ResponseEntity.notFound().build()); }
+	 */
+
+	@DeleteMapping("/{id}")
+	public Mono<ResponseEntity<Void>> delete(@PathVariable String id) {
+		return service.findById(id).flatMap(p -> {
+			return service.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
+		}).defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
 	}
 }
