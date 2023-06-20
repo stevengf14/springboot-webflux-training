16b6473ab9511535dfef25c1ad6847527b00e2ac
Steven <stevengf14@gmail.com>
2023-06-19 23:39:08 -0500

    Post handler - create

--- springboot-webflux-apirest/src/main/java/ec/com/learning/webflux/app/controllers/ProductController.java
+++ springboot-webflux-apirest/src/main/java/ec/com/learning/webflux/app/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package ec.com.learning.webflux.app.controllers;
 
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +38,14 @@ public class ProductController {
 	public Mono<ResponseEntity<Product>> get(@PathVariable String id) {
 		return service.findById(id).map(p -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(p))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public Mono<ResponseEntity<Product>> create(@RequestBody Product product) {
+		if (product.getCreateAt() == null) {
+			product.setCreateAt(new Date());
+		}
+		return service.save(product).map(p -> ResponseEntity.created(URI.create("/api/products/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8).body(p));
 	}
 }
