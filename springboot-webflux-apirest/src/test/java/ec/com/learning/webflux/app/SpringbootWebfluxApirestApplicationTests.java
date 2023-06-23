574604dd248eaa08884a871918defea7501b4dc5
Steven <stevengf14@gmail.com>
2023-06-22 23:01:33 -0500

    testing list endpoint

--- springboot-webflux-apirest/src/test/java/ec/com/learning/webflux/app/SpringbootWebfluxApirestApplicationTests.java
+++ springboot-webflux-apirest/src/test/java/ec/com/learning/webflux/app/SpringbootWebfluxApirestApplicationTests.java
@@ -1,13 +1,23 @@
 package ec.com.learning.webflux.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import ec.com.learning.webflux.app.models.documents.Product;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringbootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void listTest() {
+		client.get().uri("/api/v2/products").accept(MediaType.APPLICATION_JSON_UTF8).exchange().expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8).expectBodyList(Product.class).hasSize(12);
 	}
 
 }
