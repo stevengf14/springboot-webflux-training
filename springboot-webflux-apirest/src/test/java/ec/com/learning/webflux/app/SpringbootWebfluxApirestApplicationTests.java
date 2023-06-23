2d79309996a46702f05ee2c34992478ad57d213a
Steven <stevengf14@gmail.com>
2023-06-22 23:17:15 -0500

    consumeWith

--- springboot-webflux-apirest/src/test/java/ec/com/learning/webflux/app/SpringbootWebfluxApirestApplicationTests.java
+++ springboot-webflux-apirest/src/test/java/ec/com/learning/webflux/app/SpringbootWebfluxApirestApplicationTests.java
@@ -1,5 +1,8 @@
 package ec.com.learning.webflux.app;
 
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +20,14 @@ class SpringbootWebfluxApirestApplicationTests {
 	@Test
 	void listTest() {
 		client.get().uri("/api/v2/products").accept(MediaType.APPLICATION_JSON_UTF8).exchange().expectStatus().isOk()
-				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8).expectBodyList(Product.class).hasSize(12);
+				.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8).expectBodyList(Product.class)
+				/* .hasSize(12) */.consumeWith(response -> {
+					List<Product> products = response.getResponseBody();
+					products.forEach(p -> {
+						System.out.println(p.getName());
+					});
+					Assertions.assertThat(products.size() > 0).isTrue();
+				});
 	}
 
 }
