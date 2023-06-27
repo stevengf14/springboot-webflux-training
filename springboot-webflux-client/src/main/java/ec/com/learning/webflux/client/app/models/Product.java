33e33ce9f6633bcd3e8a785a84ba69f120044d42
Steven <stevengf14@gmail.com>
2023-06-27 00:02:37 -0500

    dto classes

---
+++ springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/models/Product.java
@@ -0,0 +1,62 @@
+package ec.com.learning.webflux.client.app.models;
+
+import java.util.Date;
+
+public class Product {
+
+	private String id;
+	private String name;
+	private String price;
+	private Date createAt;
+	private String photo;
+	private Category category;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
