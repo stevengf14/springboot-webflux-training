9b498599cbc04fbdc68a6a7d6403bbf957263ff0
Steven <stevengf14@gmail.com>
2023-06-27 22:32:20 -0500

    CRUD in the service

--- springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/service/ProductServiceImpl.java
+++ springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/service/ProductServiceImpl.java
@@ -1,16 +1,20 @@
 package ec.com.learning.webflux.client.app.service;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import static org.springframework.http.MediaType.*;
+import org.springframework.stereotype.Service;
+import static org.springframework.web.reactive.function.BodyInserters.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import ec.com.learning.webflux.client.app.models.Product;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 
 	@Autowired
@@ -18,7 +22,7 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public Flux<Product> findAll() {
-		return client.get().accept(MediaType.APPLICATION_JSON_UTF8).exchange()
+		return client.get().accept(APPLICATION_JSON_UTF8).exchange()
 				.flatMapMany(response -> response.bodyToFlux(Product.class));
 	}
 
@@ -26,28 +30,29 @@ public class ProductServiceImpl implements ProductService {
 	public Mono<Product> findById(String id) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("id", id);
-		return client.get().uri("/{id}", params).accept(MediaType.APPLICATION_JSON_UTF8).retrieve()
-				.bodyToMono(Product.class);
+		return client.get().uri("/{id}", params).accept(APPLICATION_JSON_UTF8).retrieve().bodyToMono(Product.class);
 		// .exchange()
 		// .flatMap(response -> response.bodyToMono(Product.class));
 	}
 
 	@Override
 	public Mono<Product> save(Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		return client.post().accept(APPLICATION_JSON_UTF8).contentType(APPLICATION_JSON_UTF8)
+				// .body(fromObject(product))
+				.syncBody(product).retrieve().bodyToMono(Product.class);
 	}
 
 	@Override
 	public Mono<Product> update(Product product, String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return client.put().uri("/{id}", Collections.singletonMap("id", id)).accept(APPLICATION_JSON_UTF8)
+				.contentType(APPLICATION_JSON_UTF8)
+				// .body(fromObject(product))
+				.syncBody(product).retrieve().bodyToMono(Product.class);
 	}
 
 	@Override
 	public Mono<Void> delete(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return client.delete().uri("/{id}", Collections.singletonMap("id", id)).exchange().then();
 	}
 
 }
