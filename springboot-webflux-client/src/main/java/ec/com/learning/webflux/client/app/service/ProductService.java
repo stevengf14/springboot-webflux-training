d84ff51bcd88f7cf55bfa4cd52667aa4139b25e7
Steven <stevengf14@gmail.com>
2023-06-28 00:19:04 -0500

    upload in service

--- springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/service/ProductService.java
+++ springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/service/ProductService.java
@@ -1,5 +1,7 @@
 package ec.com.learning.webflux.client.app.service;
 
+import org.springframework.http.codec.multipart.FilePart;
+
 import ec.com.learning.webflux.client.app.models.Product;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,4 +18,6 @@ public interface ProductService {
 
 	public Mono<Void> delete(String id);
 
+	public Mono<Product> upload(FilePart file, String id);
+	
 }
--- springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/service/ProductServiceImpl.java
+++ springboot-webflux-client/src/main/java/ec/com/learning/webflux/client/app/service/ProductServiceImpl.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.http.codec.multipart.FilePart;
+
 import static org.springframework.http.MediaType.*;
 import org.springframework.stereotype.Service;
 import static org.springframework.web.reactive.function.BodyInserters.*;
@@ -53,6 +57,16 @@ public class ProductServiceImpl implements ProductService {
 	@Override
 	public Mono<Void> delete(String id) {
 		return client.delete().uri("/{id}", Collections.singletonMap("id", id)).exchange().then();
+	}
+
+	@Override
+	public Mono<Product> upload(FilePart file, String id) {
+		MultipartBodyBuilder parts = new MultipartBodyBuilder();
+		parts.asyncPart("file", file.content(), DataBuffer.class).headers(h -> {
+			h.setContentDispositionFormData("file", file.filename());
+		});
+		return client.post().uri("/upload/{id}", Collections.singletonMap("id", id)).contentType(MULTIPART_FORM_DATA)
+				.syncBody(parts.build()).retrieve().bodyToMono(Product.class);
 	}
 
 }
