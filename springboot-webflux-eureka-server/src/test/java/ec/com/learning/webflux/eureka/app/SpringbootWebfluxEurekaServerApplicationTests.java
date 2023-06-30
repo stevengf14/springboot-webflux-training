20336c2235a2cd638ca093ed9021ff6b6f7d5185
Steven <stevengf14@gmail.com>
2023-06-29 22:47:02 -0500

    creating eureka server project

---
+++ springboot-webflux-eureka-server/src/test/java/ec/com/learning/webflux/eureka/app/SpringbootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.learning.webflux.eureka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
